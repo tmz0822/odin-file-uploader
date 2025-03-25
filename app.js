45a31e2b6f0ca86b9c372c6a99424664dff62d70
tmz0822 <97086526+tmz0822@users.noreply.github.com>
2025-03-25 16:50:43 +0800

    init database

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const path = require('node:path');
 const indexRouter = require('./routes/indexRouter');
+const session = require('express-session');
+const passport = require('passport');
+const authRouter = require('./routes/authRouter');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 
 const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 
 // EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Session setup
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 }, // 1 hour
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+// Passport
+require('./config/passport');
+app.use(passport.session());
+
 // Routes
 app.get('/', indexRouter);
+app.get('/', authRouter);
 
 app.listen(3000, () => {
   console.log('server listening on port 3000');
